d67cf3ebce999d2569633304d96af40de9d11597
asrashley <alex@ashley-family.net>
2013-06-05 09:12:20 +0100

    use coverage.py to check unit test coverage

---
+++ test-coverage.bat
@@ -0,0 +1,2 @@
+coverage run --source=ballot,project,timeline,util --omit=django,dbindexer,djangoappengine,djangotoolbox,autoload manage.py test project ballot project report timeline util
+coverage report 
