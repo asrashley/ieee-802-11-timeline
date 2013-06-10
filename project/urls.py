e68fff42b5362c843e91e30dbbff06ec17bacfb9
asrashley <alex@ashley-family.net>
2013-06-10 08:26:33 +0100

    moved add_project and edit_project from timeline to project

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns
 
 urlpatterns = patterns('project.views',
+                       (r'^edit_project/(?P<proj>\d+)$', 'edit_project'),
+                       (r'^delete_project/(?P<proj>\d+)$', 'del_project'),
+                       (r'^new_project/$', 'add_project'),
                        (r'^dn/$', 'backlog_worker'),
                        (r'^dn/status.json', 'backlog_poll'),
+                       (r'^$', 'main_page')
 )
