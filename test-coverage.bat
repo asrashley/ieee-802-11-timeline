f6287894578cf7d3ae6152f98f18210eafaaaa44
asrashley <alex@ashley-family.net>
2013-06-13 17:41:21 +0100

    added html output

--- test-coverage.bat
+++ test-coverage.bat
@@ -1,2 +1,3 @@
 coverage run --source=ballot,project,timeline,util --omit=django,dbindexer,djangoappengine,djangotoolbox,autoload manage.py test project ballot project report timeline util
 coverage report 
+coverage html
