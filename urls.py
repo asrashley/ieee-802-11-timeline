abb33ca62607e283537d027148c8576280049add
asrashley <alex@ashley-family.net>
2013-07-05 07:42:05 +0100

    renamed util to system. Updated to class based RedirectView

--- urls.py
+++ urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
                        (r'^timeline/', include('timeline.urls')),
                        (r'^ballot/', include('ballot.urls')),
                        (r'^project/', include('project.urls')),
-                       (r'^util/', include('util.urls')),
+                       (r'^system/', include('system.urls')),
                        (r'^report/', include('report.urls')),
                        (r'^login/$', 'django.contrib.auth.views.login'),
                        (r'^logout/$', 'django.contrib.auth.views.logout'),
                        (r'^password_change/$', 'django.contrib.auth.views.password_change'),
                        (r'^password_change/done$', 'django.contrib.auth.views.password_change_done'),
-                       ('^ah/$','django.views.generic.simple.redirect_to',{'url':'/_ah/admin/'}),
-                       ('^$', 'util.views.index_page'),
+                       ('^ah/$',RedirectView.as_view(url='/_ah/admin/')),
+                       ('^$', 'system.views.index_page'),
 )
