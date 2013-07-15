660486655f7f4fb556aafc5ee39fb8c147614035
Alex Ashley <asrashley@gmail.com>
2013-07-15 16:17:13 +0200

    added test for decimal session number and extra checks of HTML output
    Added test for the fact that sessions are now decimal fields. Added checks
    that every report is listed in the HTML output.

--- report/tests.py
+++ report/tests.py
@@ -19,57 +19,68 @@
 #  Author              :    Alex Ashley
 #
 #############################################################################
-from report.models import MeetingReport
 
-from django.test import TestCase
+import datetime
+
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
-class ReportTestBase(TestCase):
-    def _check_page(self,url):
-        print url
-        response = self.client.get(url)
-        # not logged in, should redirect to login page
-        self.failUnlessEqual(response.status_code, 302)
+from report.models import MeetingReport
+from util.tests import LoginBasedTest
 
-        login = self.client.login(username='test', password='password')
-        self.failUnless(login, 'Could not log in')
-        response = self.client.get(url)
-        self.failUnlessEqual(response.status_code, 200)
-        return response
-        
-class EmptyReportTest(ReportTestBase):
+class EmptyReportTest(LoginBasedTest):
     fixtures = ['site.json']
     def test_report(self):
-        from django.conf import settings
         static_url = settings.STATICFILES_URL
         url = reverse('report.views.main_page',args=[])
         response = self._check_page(url)
         self.assertContains(response, 'ieeel.gif')
         response.content.index(static_url)
+        m = MeetingReport(session=123.5, 
+                          start=datetime.date(2013,1,1), 
+                          end=datetime.date(2013,1,8),
+                          pending=False,
+                          report='http://example.net/report.pdf',
+                          minutes_doc='http://example.net/minutes.doc',
+                          minutes_pdf='http://example.net/minutes.pdf',
+                          venue='Waikoloa HI USA',
+                          location='Hilton Waikoloa Village',
+                          meeting_type=MeetingReport.Plenary.code
+                          )
+        m.save()
+        m2 = MeetingReport.objects.get(session=123.5)
         
     def test_export(self):
-        from django.conf import settings
         static_url = settings.STATICFILES_URL
         url = ''.join([reverse('report.views.main_page',args=[]),'meeting-reports.html'])
         response = self._check_page(url)
         self.assertContains(response, 'ieeel.gif')
         self.failUnlessRaises(ValueError, response.content.index,static_url)
 
-class ReportTest(ReportTestBase):
-    from django.conf import settings
+class ReportTest(LoginBasedTest):
     static_url = settings.STATICFILES_URL
     fixtures = ['site.json','reports.json']
     
+    def _check_response(self,response):
+        fields = ['session_num','report','minutes_doc','minutes_pdf','venue','location']
+        for report in MeetingReport.objects.all():
+            for field in fields:
+                val = getattr(report, field)
+                if val:
+                    self.assertContains(response, val, msg_prefix=field)
+        
     def test_report(self):
         self.assertGreater(MeetingReport.objects.count(), 0, "Failed to load reports.json fixture")
         url = reverse('report.views.main_page',args=[])
         response = self._check_page(url)
         self.assertContains(response, 'ieeel.gif')
         response.content.index(self.static_url)
-        
+        self._check_response(response)
+    
     def test_export(self):
         self.assertGreater(MeetingReport.objects.count(), 0, "Failed to load reports.json fixture")
         url = ''.join([reverse('report.views.main_page',args=[]),'meeting-reports.html'])
         response = self._check_page(url)
         self.assertContains(response, 'ieeel.gif')
         self.failUnlessRaises(ValueError, response.content.index,self.static_url)
+        self._check_response(response)
